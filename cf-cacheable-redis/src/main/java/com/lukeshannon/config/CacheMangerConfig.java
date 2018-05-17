76c2683ec07b68fea476040157820f2d44aa3c8a
lshannon <luke.shannon@gmail.com>
2018-05-17 15:17:14 -0400

    Updated to Spring Boot

--- cf-cacheable-redis/src/main/java/com/lukeshannon/config/CacheMangerConfig.java
+++ cf-cacheable-redis/src/main/java/com/lukeshannon/config/CacheMangerConfig.java
@@ -3,46 +3,23 @@
  */
 package com.lukeshannon.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
-import org.springframework.cloud.Cloud;
-import org.springframework.cloud.CloudFactory;
-import org.springframework.cloud.service.common.RedisServiceInfo;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 
 /**
  * @author lshannon
  *
  */
+@Configuration
 public class CacheMangerConfig extends CachingConfigurerSupport {
-	
-	@Value("${redis-service-name}")
-	private String redisServiceName;
-	
-	@Bean
-	  public RedisConnectionFactory redisConnectionFactory() {
-		CloudFactory cloudFactory = new CloudFactory();
-        Cloud cloud = cloudFactory.getCloud();
-        RedisServiceInfo serviceInfo = (RedisServiceInfo) cloud.getServiceInfo(redisServiceName);
-        String serviceID = serviceInfo.getId();
-        return cloud.getServiceConnector(serviceID, RedisConnectionFactory.class, null);
-	  }
 
 	  @Bean
-	  public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory cf) {
-	    RedisTemplate<String, String> redisTemplate = new RedisTemplate<String, String>();
-	    redisTemplate.setConnectionFactory(cf);
-	    return redisTemplate;
-	  }
-
-	  @Bean
-	  public CacheManager cacheManager(RedisTemplate redisTemplate) {
-	    RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
-	    cacheManager.setDefaultExpiration(300);
+	  public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+	    RedisCacheManager cacheManager = RedisCacheManager.create(redisConnectionFactory);
 	    return cacheManager;
 	  }
 
